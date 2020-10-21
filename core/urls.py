91deaef09ed4f42d6c992705c77dae4f6c02e27d
fmammadli <mammedli.fidan2000@gmail.com>
2020-10-21 21:40:50 +0400

    message

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from core.views import index, product, login, basket, checkout, sign_up, \
+from core.views import index, product, login, basket, checkout, sign_up,logoutUser, \
     BonuseListView
 
 urlpatterns = [
     path('', index, name='home'),
     path('product/', product, name='product'),
     path('login/', login, name='login'),
+    path('logout/', logoutUser, name='logout'),
     path('basket/', basket, name='basket'),
     path('checkout/', checkout, name='checkout'),
     path('sign_up/', sign_up, name='sign_up'),
--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView
-
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login as auth_login,logout
+from django.contrib.auth.decorators import login_required   
 from core.models import Bonuse
 
 
@@ -10,9 +14,12 @@ def index(request):
 def product(request):
     return render(request,'product.html')
 
-def login(request):
-    return render(request,'login.html')
 
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+@login_required(login_url='login')
 
 def basket(request):
     return render(request ,'basket.html')
@@ -21,7 +28,41 @@ def checkout(request):
     return render(request ,'checkout.html')
 
 def sign_up(request):
-    return render(request ,'sign_up.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form=CreateUserForm()
+
+        if request.method=='POST':
+            form=CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request,'Account was created for' + user)
+                return redirect('login')
+
+
+
+        context={'form':form}
+        return render(request ,'sign_up.html',context)
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method=='POST':
+            print(request.POST)
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(request,username=username,password=password)
+            if user is not None:
+                auth_login(request,user)
+                return redirect('home')
+            else:
+                messages.info(request,'Email or password is incorrect')
+
+
+        return render(request,'login.html')
 
 # def bonuses(request):
 #     return render(request, 'membership_card.html')
